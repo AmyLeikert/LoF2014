95b4cee7e6064e07e521827273676607c411c3e6
Amy Leikert <amy.leikert@detroitlabs.com>
2014-06-16 21:02:54 -0400

    Initial Commit

---
+++ LakesKraken/ALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAppDelegate.h
+//  LakesKraken
+//
+//  Created by DetroitLabsUser on 6/16/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LakesKrakenTests/LakesKrakenTests.m
@@ -0,0 +1,34 @@
+//
+//  LakesKrakenTests.m
+//  LakesKrakenTests
+//
+//  Created by DetroitLabsUser on 6/16/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LakesKrakenTests : XCTestCase
+
+@end
+
+@implementation LakesKrakenTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
