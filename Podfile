5bbaa8e7bd7d436418a5cacbf17243f25c3012c3
Amy Leikert <amy.leikert@detroitlabs.com>
2014-08-22 12:37:07 -0400

    added facebook cocoapod

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ platform :ios, "7.0"
 target "LakesKraken" do
   pod 'BlinkButton', '~> 0.0'
   pod 'EXPhotoViewer', '~> 1.1'
+  pod 'Facebook-iOS-SDK', '~> 3.17'
 end
---
+++ Pods/Pods-LakesKraken-Bolts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LakesKraken_Bolts : NSObject
+@end
+@implementation PodsDummy_Pods_LakesKraken_Bolts
+@end
