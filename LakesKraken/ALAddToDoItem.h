2b309f0c13adfb83e2d51aaa2a3396ea95f70aa9
Amy Leikert <amy.leikert@detroitlabs.com>
2014-09-09 15:57:19 -0400

    Added WhatToPack views

---
+++ LakesKraken/ALAddToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  ALAddToDoItem.h
+//  
+//
+//  Created by DetroitLabsUser on 9/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAddToDoItem : UIViewController
+
+@end
---
+++ LakesKraken/ALAddToDoItem.m
@@ -0,0 +1,50 @@
+//
+//  ALAddToDoItem.m
+//  
+//
+//  Created by DetroitLabsUser on 9/8/14.
+//
+//
+
+#import "ALAddToDoItem.h"
+
+@interface ALAddToDoItem ()
+
+@end
+
+@implementation ALAddToDoItem
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.navigationController.navigationBar.translucent = NO;
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
