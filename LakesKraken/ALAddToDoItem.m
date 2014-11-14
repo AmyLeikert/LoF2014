efa258aee4a6061bc1d44aae738a8257a6cd2409
amyleikert <amy.leikert@gmail.com>
2014-11-14 15:06:19 -0500

    AddToDoItem layout fixed

--- LakesKraken/ALAddToDoItem.m
+++ LakesKraken/ALAddToDoItem.m
@@ -8,6 +8,7 @@
 
 #import "ALAddToDoItem.h"
 #import "ALToDoTable.h"
+#import "menuViewController.h"
 
 @interface ALAddToDoItem ()
 
@@ -27,6 +28,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.textField.borderStyle = UITextBorderStyleNone;
+    [self.textField setBackground:[UIImage imageNamed:@"bigWoodRectangle.png"]];
     
     self.view.backgroundColor = [UIColor colorWithRed:1 green:0.937 blue:0.78 alpha:1];
     self.navigationController.navigationBar.translucent = NO;
@@ -49,7 +52,45 @@
     [super didReceiveMemoryWarning];
 }
 
-- (IBAction)cancel:(id)sender {
+- (IBAction)save:(id)sender {
+    NSString *name = self.textField.text;
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Entity" inManagedObjectContext:self.managedObjectContext];
+    
+    // Initialize Record
+    NSManagedObject *record = [[NSManagedObject alloc] initWithEntity:entity insertIntoManagedObjectContext:self.managedObjectContext];
+    
+    if (name && name.length) {
+        
+        // Populate Record
+        [record setValue:name forKey:@"name"];
+        [record setValue:[NSDate date] forKey:@"createdAt"];
+        
+        // Save Record
+        NSError *error = nil;
+        
+        ALToDoTable *toDoList = [[ALToDoTable alloc]init];
+        [self.navigationController pushViewController:toDoList animated:YES];
+        
+        if ([self.managedObjectContext save:&error]) {
+            // Dismiss View Controller
+            [self dismissViewControllerAnimated:YES completion:nil];
+            
+        } else {
+            if (error) {
+                NSLog(@"Unable to save record.");
+                NSLog(@"%@, %@", error, error.localizedDescription);
+            }
+            
+            // Show Alert View
+            [[[UIAlertView alloc] initWithTitle:@"Warning" message:@"Your to-do could not be saved." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+        }
+        
+    } else {
+        // Show Alert View
+        [[[UIAlertView alloc] initWithTitle:@"Warning" message:@"Your to-do needs a name." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+
     
 }
 
@@ -95,5 +136,19 @@
     
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO];
+}
+
+- (IBAction)backTouched:(id)sender {
+     ALToDoTable *menu = [[ALToDoTable alloc]init];
+    [self.navigationController pushViewController:menu animated:YES];
+}
 
 @end
