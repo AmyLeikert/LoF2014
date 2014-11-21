f49c2744b83288d4196f694a512fcbb5a77a1d24
amyleikert <amy.leikert@gmail.com>
2014-11-21 15:44:25 -0500

    principle descriptions

--- LakesKraken/ALPrincipleDescriptionController.m
+++ LakesKraken/ALPrincipleDescriptionController.m
@@ -20,24 +20,17 @@
     [self.nameLabel setText:self.princName];
     
     [self grabDescription];
-
-    // Do any additional setup after loading the view from its nib.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:1 green:0.937 blue:0.78 alpha:1];
+    [self textViewDidChange:self.descripText];
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 -(void)grabDescription {
     
@@ -45,7 +38,33 @@
         self.descripText.text = @"Anyone may be a part of Lakes of Fire. We welcome and respect the stranger. No prerequisites exist for participation in our community."; }
     if ([self.princName isEqualToString: @"Radical Self-Reliance"]) {
         self.descripText.text = @"Lakes of Fire encourages the individual to discover, exercise and rely on his or her inner resources."; }
+    if ([self.princName isEqualToString: @"Gifting"]) {
+        self.descripText.text = @"Lakes of Fire is devoted to acts of gift giving. The value of a gift is unconditional. Gifting does not contemplate a return or an exchange for something of equal value."; }
+    if ([self.princName isEqualToString: @"Decommodification"]) {
+        self.descripText.text = @"In order to preserve the spirit of gifting, our community seeks to create social environments that are unmediated by commercial sponsorships, transactions, or advertising. We stand ready to protect our culture from such exploitation. We resist the substitution of consumption for participatory experience."; }
+    if ([self.princName isEqualToString: @"Radical Self-Expression"]) {
+        self.descripText.text = @"Radical self-expression arises from the unique gifts of the individual. No one other than the individual or a collaborating group can determine its content. It is offered as a gift to others. In this spirit, the giver should respect the rights and liberties of the recipient."; }
+    if ([self.princName isEqualToString: @"Communal Effort"]) {
+        self.descripText.text = @"Our community values creative cooperation and collaboration. We strive to produce, promote and protect social networks, public spaces, works of art, and methods of communication that support such interaction."; }
+    if ([self.princName isEqualToString: @"Civic Responsibility"]) {
+        self.descripText.text = @"We value civil society. Community members who organize events should assume responsibility for public welfare and endeavor to communicate civic responsibilities to participants. They must also assume responsibility for conducting events in accordance with local, state and federal laws."; }
+    if ([self.princName isEqualToString: @"Leave No Trace"]) {
+        self.descripText.text = @"Our community respects the environment. We are committed to leaving no physical trace of our activities wherever we gather. We clean up after ourselves and endeavor, whenever possible, to leave such places in a better state than when we found them."; }
     
+    if ([self.princName isEqualToString: @"Participation"]) {
+        self.descripText.text = @"Our community is committed to a radically participatory ethic. We believe that transformative change, whether in the individual or in society, can occur only through the medium of deeply personal participation. We achieve being through doing. Everyone is invited to work. Everyone is invited to play. We make the world real through actions that open the heart."; }
+    if ([self.princName isEqualToString: @"Immediacy"]) {
+        self.descripText.text = @"Immediate experience is, in many ways, the most important touchstone of value in our culture. We seek to overcome barriers that stand between us and a recognition of our inner selves, the reality of those around us, participation in society, and contact with a natural world exceeding human powers. No idea can substitute for this experience."; }
+    
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    CGFloat fixedWidth = textView.frame.size.width;
+    CGSize newSize = [textView sizeThatFits:CGSizeMake(fixedWidth, MAXFLOAT)];
+    CGRect newFrame = textView.frame;
+    newFrame.size = CGSizeMake(fmaxf(newSize.width, fixedWidth), newSize.height);
+    textView.frame = newFrame;
 }
 
 @end
--- LakesKraken/ALPrinciplesViewController.m
+++ LakesKraken/ALPrinciplesViewController.m
@@ -13,13 +13,15 @@
 
 @end
 
-//NSString* const cellReuseIdentifier = @"cellIdentifier";
 @implementation ALPrinciplesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self.table registerClass: [UITableViewCell class] forCellReuseIdentifier:@"cellReuseIdentifier"];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:1 green:0.937 blue:0.78 alpha:1];
+    self.table.backgroundColor = [UIColor colorWithRed:1 green:0.937 blue:0.78 alpha:1];
     
     NSString *stream = @"https://www.youtube.com/watch?v=ysl-Yu4Rhdw";
     NSURL *url = [NSURL URLWithString:stream];
@@ -45,18 +47,7 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     
@@ -69,9 +60,7 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    ALPrincipleDescriptionController *descripView = [[ALPrincipleDescriptionController alloc]init];
-    
+
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellReuseIdentifier" forIndexPath:indexPath];
     
     if (!cell) {
